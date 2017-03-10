2fe2d4bd6036a00a7cca106860132350a565e1f4
tobrom <tobias_romar@hotmail.com>
2017-03-10 17:53:19 +0100

    Update 2017-03-10

--- server.R
+++ server.R
@@ -6,6 +6,7 @@ library(smooth)
 library(reshape2)
 require(scales)
 library(TTR)
+Sys.setlocale(category = "LC_TIME", locale="us")
 
 
 shinyServer(function(input, output) {
@@ -38,23 +39,12 @@ shinyServer(function(input, output) {
 })
   
 
-  
 
-  
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
   
   
   monthlyTimeData <- reactive({
     
+   
     monthlyData <- dailyData()
     monthlyData$Date <- as.Date(monthlyData$Date) 
     monthlyData$month <- format(as.Date(monthlyData$Date), "%m")
@@ -66,6 +56,8 @@ shinyServer(function(input, output) {
       merge(monthlyData, by = c("Date", "year", "month")) %>% 
       as.data.frame()
     
+    monthlyDataRaw <<- monthlyData
+    
     ##Creating The time series
     ts(monthlyData$Adj.Close, 
        start = c(as.numeric(monthlyData$year[1]),
@@ -76,12 +68,104 @@ shinyServer(function(input, output) {
   })
   
   
+  output$plot12 <- renderPlot({
+    
+    data2 <- dailyData()
+    #data2 <- t
+    
+    data2 <- data2 %>% 
+      mutate(shortMA = SMA(Adj.Close, 50),longMA = SMA(Adj.Close, 200)) %>% 
+      select(Date, Adj.Close, shortMA, longMA) %>% as.data.frame() 
+    
+    colnames(data2)[2] <- "Price"
+    colnames(data2)[3] <- "MA(50)" 
+    colnames(data2)[4] <- "MA(200)"
+    
+    maLong <<- tail(data2, 1)
+    
+    data2 <- melt(data2, id = "Date")
+    
+    ggplot(subset(data2, as.Date(Date) > Sys.Date()-500), aes(as.Date(Date), value, color = variable)) + 
+      geom_line(size = 0.8) + 
+      labs(x = "Date",
+           y = "Daily Closing Price") +
+      scale_x_date(breaks = date_breaks("3 months"),
+                   labels = date_format("%Y-%m")) +
+      scale_colour_manual(values = c("#000000", 
+                                     "#0000FF", 
+                                     "#B0C4DE")) +
+      theme(panel.background = element_blank(),
+            plot.background = element_blank(),
+            legend.title=element_blank(),
+            legend.key = element_blank(),
+            panel.grid.major.y = element_line(colour = "grey", size = 0.1, linetype = 20),
+            plot.title = element_text(size = rel(1.5)))
+    
+    
+  })    
+  
+  
+  
+  output$plot2 <- renderPlot({
+    
+    data2 <- dailyData()
+    
+    data2 <- data2 %>% 
+      mutate(shortMA = SMA(Adj.Close, input$shortMA),longMA = SMA(Adj.Close, input$longMA)) %>% 
+      select(Date, Adj.Close, shortMA, longMA) %>% as.data.frame() 
+    
+    colnames(data2)[2] <- "Price"
+    colnames(data2)[3] <- paste0("MA(",input$shortMA,")") 
+    colnames(data2)[4] <- paste0("MA(",input$longMA,")")
+    
+    maShort <<- tail(data2, 1)
+    
+    data2 <- melt(data2, id = "Date")
+    
+    ggplot(subset(data2, as.Date(Date) > Sys.Date()-100), aes(as.Date(Date), value, color = variable)) + 
+      geom_line(size = 0.8) + 
+      labs(x = "Date",
+           y = "Daily Closing Price") +
+      scale_x_date(breaks = date_breaks("months"),
+                   labels = date_format("%Y-%m")) +
+      scale_colour_manual(values = c("#000000", 
+                                     "#0000FF", 
+                                     "#B0C4DE")) +
+      theme(panel.background = element_blank(),
+            plot.background = element_blank(),
+            legend.title=element_blank(),
+            legend.key = element_blank(),
+            panel.grid.major.y = element_line(colour = "grey", size = 0.1, linetype = 20),
+            plot.title = element_text(size = rel(1.5)))
+    
+    
+  })  
+  
+  
+  output$plot3 <- renderPlot({
+    
+    ts.stock <- monthlyTimeData()
+    
+    ###Decomposing the time series
+    decom <- stl(ts.stock, s.window = "period")
+    seasonData <<- decom
+    
+    autoplot(decom) +  
+      labs(x = "Year",
+           y = "") +
+      theme(panel.background = element_blank(),
+            plot.background = element_blank(),
+            panel.grid.major.y = element_line(colour = "grey", size = 0.1, linetype = 20),
+            plot.title = element_text(size = rel(1.5)))
+    
+    
+  })   
   
   
   
   
   textData <- reactive({
-   #data <- dailyData
+ 
     data <- dailyData()
     
     maxDate <- max(as.Date(data$Date))
@@ -94,23 +178,74 @@ shinyServer(function(input, output) {
                       as.Date(data[(format(as.Date(data$Date), "%Y") == (as.numeric(
                         max(format(as.Date(data$Date), "%Y"))) - 1)),][,1])))),][7])
     
-    ytdReturn <- round((as.numeric(lastClose)/as.numeric(prevClose)-1)*100, digits = 1)
-    totalReturn <- round((as.numeric(lastClose)/as.numeric(firstClose)-1)*100, digits = 1)
+    ytdReturn <- (as.numeric(lastClose)/as.numeric(prevClose)-1)*100
+    totalReturn <- (as.numeric(lastClose)/as.numeric(firstClose)-1)*100
     
     ytdText <- ifelse(ytdReturn > 0, "increase", "decrease")
     
+##########    
+    
+    longTrend <- maLong
+    
+    longAboveBelow <- ifelse(longTrend[,3] > longTrend[,4], "above", "below")
+    longPosNeg <- ifelse(longTrend[,3] > longTrend[,4], "positive", "negative")
     
     
     
+    shortTrend <- maShort
+    
+    shortAboveBelow <- ifelse(shortTrend[,3] > shortTrend[,4], "above", "below")
+    shortPosNeg <- ifelse(shortTrend[,3] > shortTrend[,4], "positive", "negative")
+    
+    sameTrueFalse <- ifelse(longPosNeg == shortPosNeg, "The same", "However, the opposite")
+    
+    seasonData <- seasonData
+    monthlyDataRaw <- monthlyDataRaw
+    
+    season <- head(cbind(monthlyDataRaw, data.frame(seasonData$time.series)), 12)
+    season$monthName <-  format(season$Date, "%B")
+    season <- season[order(-season$seasonal),]
+    
+    bestMonths <- paste0(head(season$monthName, 2), collapse = " and ")
+    worstMonths <- paste0(tail(season$monthName, 2), collapse = " and ")
+    
+##########    
     
     
-   data.frame(company = paste(input$tickers),
-               minDate = minDate,
-             maxDate = maxDate,
-               lastClose = lastClose,
-               ytdReturn = ytdReturn,
-               totalReturn = totalReturn,
-               ytdText = ytdText)
+    expModelIn <- expModelOut
+    expModelIn$yearMonth <- row.names(expModelIn)
+    
+    arimaModelIn <- arimaModelOut
+    arimaModelIn$yearMonth <- row.names(arimaModelIn)
+    
+    expTarget <- tail(expModelIn[,1], 1)
+    expTargetMonth <- tail(expModelIn[,6], 1)
+    expPotential <- (as.numeric(expTarget)/as.numeric(lastClose)-1)*100
+    
+    
+    arimaTarget <- tail(arimaModelIn[,1], 1)
+    arimaPotential <- (as.numeric(arimaTarget)/as.numeric(lastClose)-1)*100
+    
+    
+      data.frame(company = paste(input$tickers),
+              minDate = minDate,
+              maxDate = maxDate,
+              lastClose = format(round(as.numeric(lastClose), digits = 2), nsmall = 2),
+              ytdReturn = paste(format(round(ytdReturn, digits = 1), nsmall = 1), "%"),
+              totalReturn = paste(format(round(totalReturn, digits = 1), nsmall = 1), "%"),
+              ytdText = ytdText,
+              longAboveBelow = longAboveBelow,
+              longPosNeg = longPosNeg,
+              shortAboveBelow = shortAboveBelow,
+              shortPosNeg = shortPosNeg,
+              sameTrueFalse = sameTrueFalse,
+              bestMonths = bestMonths,
+              worstMonths = worstMonths,
+              expTarget = format(round(as.numeric(expTarget), digits = 2), nsmall = 2),
+              expTargetMonth = expTargetMonth,
+              expPotential = paste(format(round(expPotential, digits = 1), nsmall = 1), "%"),
+              arimaTarget = format(round(as.numeric(arimaTarget), digits = 2), nsmall = 2),
+              arimaPotential = paste(format(round(arimaPotential, digits = 1), nsmall = 1), "%"))
     
   })
   
@@ -137,59 +272,49 @@ shinyServer(function(input, output) {
     
   })  
   
-  
-  
-output$plot2 <- renderPlot({
-
-      data2 <- dailyData()
+ 
+  output$plot12 <- renderPlot({
     
-      data2 <- data2 %>% 
-        mutate(shortMA = SMA(Adj.Close, input$shortMA),longMA = SMA(Adj.Close, input$longMA)) %>% 
-        select(Date, Adj.Close, shortMA, longMA) %>% as.data.frame() 
-
-      colnames(data2)[2] <- "Price"
-      colnames(data2)[3] <- paste0("MA(",input$shortMA,")") 
-      colnames(data2)[4] <- paste0("MA(",input$longMA,")")
-      
-      data2 <- melt(data2, id = "Date")
-     
-      ggplot(subset(data2, as.Date(Date) > Sys.Date()-100), aes(as.Date(Date), value, color = variable)) + 
-        geom_line(size = 1) + 
-        labs(x = "Date",
-             y = "Daily Closing Price") +
-        scale_colour_manual(values = c("#000000", 
-                                       "#0000FF", 
-                                       "#B0C4DE")) +
-        theme(panel.background = element_blank(),
-              plot.background = element_blank(),
-              legend.title=element_blank(),
-              legend.key = element_blank(),
-              panel.grid.major.y = element_line(colour = "grey", size = 0.1, linetype = 20),
-              plot.title = element_text(size = rel(1.5)))
-           
+    data2 <- dailyData()
+    #data2 <- t
     
-  })  
-  
-  
-output$plot3 <- renderPlot({
+    data2 <- data2 %>% 
+      mutate(shortMA = SMA(Adj.Close, 50),longMA = SMA(Adj.Close, 200)) %>% 
+      select(Date, Adj.Close, shortMA, longMA) %>% as.data.frame() 
     
-    ts.stock <- monthlyTimeData()
+    colnames(data2)[2] <- "Price"
+    colnames(data2)[3] <- "MA(50)" 
+    colnames(data2)[4] <- "MA(200)"
     
-    ###Decomposing the time series
-    decom <- stl(ts.stock, s.window = "period")
+    maLong <<- tail(data2, 1)
     
+    data2 <- melt(data2, id = "Date")
     
-    autoplot(decom) +  
-      labs(x = "Year",
-           y = "") +
+    ggplot(subset(data2, as.Date(Date) > Sys.Date()-500), aes(as.Date(Date), value, color = variable)) + 
+      geom_line(size = 0.8) + 
+      labs(x = "Date",
+           y = "Daily Closing Price") +
+      scale_x_date(breaks = date_breaks("3 months"),
+                   labels = date_format("%Y-%m")) +
+      scale_colour_manual(values = c("#000000", 
+                                     "#0000FF", 
+                                     "#B0C4DE")) +
       theme(panel.background = element_blank(),
             plot.background = element_blank(),
+            legend.title=element_blank(),
+            legend.key = element_blank(),
             panel.grid.major.y = element_line(colour = "grey", size = 0.1, linetype = 20),
             plot.title = element_text(size = rel(1.5)))
     
     
-  })   
+  })    
   
+  
+   
+  
+
+  
+
   
   output$plot4 <- renderPlot({
     
@@ -201,6 +326,7 @@ output$plot3 <- renderPlot({
     fit1 <- ets(ts.stock1, model = "ZZZ")
     
     pred1 <- forecast(fit1,input$period)
+    expModelOut <<- data.frame(pred1)
     
     autoplot(pred1) +labs(x = "Year",
                          y = "Monthly Closing Price",
@@ -223,6 +349,7 @@ output$plot3 <- renderPlot({
     fit2 <- auto.arima(ts.stock2)
     
     pred2 <- forecast(fit2, input$period)
+    arimaModelOut <<- data.frame(pred2)
     
     #tes <<- paste(forecast(fit2)$method)
     
@@ -240,16 +367,64 @@ output$plot3 <- renderPlot({
     
     td <- textData()
     
-    sprintf("The last closing price SEK %s (%s) for %s indicates a year-to-date share 
-            price %s with %s percentage. The share price performance for the whole period (since %s) has been %s percentage", 
+    sprintf("Based on the last closing price %s (%s) for %s, the year-to-date share price  %s has been %s and 
+            the share price performance since %s has been %s", 
             td$lastClose, td$maxDate, td$company, td$ytdText, td$ytdReturn, td$minDate, td$totalReturn)
        
   
     
     }) 
   
+  
+  output$text2 <- renderText({
+    
+    td <- textData()
+    sprintf("Currently the 50 day moving average for %s is %s the 200 day moving average 
+    indicating that the share is in a %s long term trend. %s applies for the short term 
+    trend where the %s day moving average is %s the %s day moving average.", 
+            td$company, td$longAboveBelow, td$longPosNeg, td$sameTrueFalse, input$shortMA, td$shortAboveBelow, input$longMA)
+
+    
+    
+    
+  }) 
+  
+  
+  output$text3 <- renderText({
+    
+    td <- textData()
+   
+    sprintf("After decomposing the time series there are tendencies of %s being the strongest months for %s. 
+            The worst performing months have on the other hand been %s", td$bestMonths, td$company, td$worstMonths)  
+    
+    
+    
+  }) 
+  
+  
+  
+  output$text4 <- renderText({
+    
+    td <- textData()
+    
+    sprintf("The monthly closing prices for %s have been modeled with both exponetial and ARIMA models. For 
+            each of these methods, the best model based on historic data has been chosen and used for prediction. 
+            The best exponential model was chosen by minimizing the log-likelihood and the best ARIMA model 
+            was picked based on the lowest AIC. Based on the best exponetial model, the target price for %s is 
+            %s. The target price for the same period based on the best ARIMA model is %s. Based on these 
+            prediction models, the potential for the share is %s and %s respectively.", 
+            td$company, td$expTargetMonth, td$expTarget, td$arimaTarget, td$expPotential, td$arimaPotential)  
+    
+    
+    
+  }) 
+  
+  
+  
+  
 })
   
+
 
 
 
--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ symbols$yahooTicker <- paste0(gsub(" ", "-", symbols$SecuritySymbol), ".ST")
 shinyUI(fluidPage(
   
  
-  titlePanel("Time Series Analysis"),
+  titlePanel("Share Price Analysis"),
   
   hr(),
   sidebarLayout(
@@ -16,15 +16,15 @@ shinyUI(fluidPage(
       
       helpText("The application allows you to select a OMXS30 share and analyse both the long term and short time trends. 
                 In addition, monthly closing prices are decomposed into a trend component, a seasonal component and the 
-                remaining residuals. Finally, the best exponential model as well as the best ARIMA model are fitted on 
+                irregular component by loess smoothing. Finally, the best exponential model as well as the best ARIMA model are fitted on 
                 the historic data and used to predict future share prices."),
       
       hr(),
       selectInput("tickers", "Ticker:", choices = symbols$CompanyName),
-      dateInput('startDate', label = 'Start Date:', value = "2012-01-15"),
+      dateInput('startDate', label = 'Start Date:', value = "2005-01-15"),
       
       hr(),
-      
+  
       sliderInput("shortMA",
                   "Moving Average (Short):",
                   min = 5,
@@ -48,31 +48,51 @@ shinyUI(fluidPage(
       
       submitButton("Analyse"),
       hr(),
-      h3("Summary"),
-      textOutput("text1")
-     
+      
+      h3("Analysis"),
+      hr(),
+      h4("Overview"),
+      textOutput("text1"),
+      hr(),
+      h4("Current Trends"),
+      textOutput("text2"),
+      hr(),
+      h4("Seasonality"),
+      textOutput("text3"),
+      hr(),
+      h4("Prediction"),
+      textOutput("text4")
     
    ),
     
     mainPanel(
-      h3("Long Term Share Price Development"),
+      h3("Historical Price Development"),
       h6("Historical daily closing prices smoothed with LOESS"),
        plotOutput("plot1"), 
-      h3("Short Term Price Trends"),
-      h6("Current share price compared to moving averages"),
+      
+      h3("Long Term Trend - MA(50) vs. MA(200)"),
+      h6("Current share price compared to long term moving averages"),
+      
+      plotOutput("plot12"),
+      #textOutput("text2"),
+      
+      h3("Short Term Trend - Chosen Period"),
+      h6("Current share price compared to short term (user defined) moving averages"),
       
       plotOutput("plot2"),
-      #verbatimTextOutput("summary"),
-      h3("Monthly Share Price Decomposition"),
+      
+      
+      
+       h3("Price Decomposition"),
       h6("Monthly closing prices have been decomposed into a trend, a seasonal component and the remaining residuals"), 
       
       
       plotOutput("plot3"),
-      h3("Share Price Prediction - Exponential Model"),
-      h6("Based on the monthly history an optimal exponential model has been used for forecasting"),
+      h3("Prediction - Exponential Model"),
+      h6("Based on the monthly history an optimal exponential model has been used for prediction"),
        plotOutput("plot4"),
-      h3("Share Price Prediction - ARIMA Model"),
-      h6("Based on the monthly history an optimal ARIMA model has been used for forecasting"),
+      h3("Prediction - ARIMA Model"),
+      h6("Based on the monthly history an optimal ARIMA model has been used for prediction"),
       
        plotOutput("plot5")
    
